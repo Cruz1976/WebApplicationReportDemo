0ce06e362908cda9b357cd1e3f05da0d8a0d4802
Optimumcare4u <optimumcare4u@outlook.com>
2022-11-19 09:39:26 +0000

    Add project files.

---
+++ WebApplicationReportNet7/Controllers/ReportsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
+using WebApplicationReportNet7.Data;
+
+namespace WebApplicationReportNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        [HttpGet("GetPdfFile")]
+        public ActionResult GetPdfFile()
+        {
+
+            List<ReportItem> ResultAccountInvoice = new List<ReportItem>();
+
+            var rom1 = new ReportItem { Description = "Widget 6000", Price = 104.99m, Qty = 1 };
+            var rom2 = new ReportItem { Description = "Gizmo MAX", Price = 1.41m, Qty = 25 };
+            ResultAccountInvoice.Add(rom1);
+            ResultAccountInvoice.Add(rom2);
+
+
+            // var rv = new ReportViewer();
+            // var appPath = Directory.GetCurrentDirectory();
+            LocalReport localReport = new();
+            var appPath = Directory.GetCurrentDirectory();
+
+            var pathLocation = appPath + @"/Data/Report.rdlc";
+            localReport.ReportPath = pathLocation;
+            localReport.DataSources.Add(new ReportDataSource("Items", ResultAccountInvoice));
+            var pdf = localReport.Render("pdf");
+            return File(pdf, "application/pdf", "report." + "pdf");
+        }
+        [HttpGet("GetPdfFileNew")]
+        public ActionResult GetPdfFileNew()
+        {
+
+
+            List<CashReciptReport> ResultAccountInvoice = new List<CashReciptReport>();
+
+            var rom = new CashReciptReport()
+            {
+                InvoiceNo = 123,
+                CustomerAC = "Test 123",
+                OrderAddress = "123 High Road",
+                InvoiceDate = DateTime.Now,
+                TotalValue = 1000,
+                VATValue = 100,
+                TotalVAT = 1100,
+                JobAddress = "125 High Road"
+            };
+            ResultAccountInvoice.Add(rom);
+            var rom2 = new CashReciptReport()
+            {
+                InvoiceNo = 124,
+                CustomerAC = "Test 124",
+                OrderAddress = "124 High Road",
+                InvoiceDate = DateTime.Now,
+                TotalValue = 1500,
+                VATValue = 150,
+                TotalVAT = 1650,
+                JobAddress = "128 High Road"
+            };
+            ResultAccountInvoice.Add(rom2);
+
+            // var rv = new ReportViewer();
+            // var appPath = Directory.GetCurrentDirectory();
+            LocalReport localReport = new();
+            var appPath = Directory.GetCurrentDirectory();
+
+            var pathLocation = appPath + @"/Data/ReportStatement.rdlc";
+            localReport.ReportPath = pathLocation;
+            localReport.DataSources.Add(new ReportDataSource("dsCashInvoice", ResultAccountInvoice));
+            var pdf = localReport.Render("pdf");
+            return File(pdf, "application/pdf", "report." + "pdf");
+        }
+
+    }
+}
---
+++ WebApplicationReportNet7/Data/CashReciptReport.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplicationReportNet7.Data
+{
+    public class CashReciptReport
+    {
+        public int InvoiceNo { get; set; }
+        public string CustomerAC { get; set; }
+        public string OrderAddress { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal TotalValue { get; set; }
+        public decimal VATValue { get; set; }
+        public decimal TotalVAT { get; set; }
+        public string JobAddress { get; set; }
+        public string OrderNumber { get; set; }
+        public decimal Deposit { get; set; }
+        public decimal Balance { get; set; }
+
+        // Invoice Details
+        public int Quantity { get; set; }
+        public string ProductID { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int QTYOrder { get; set; }
+        public double Discount { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
